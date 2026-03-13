06880b914572416151b0eeff94fb892c5f4ddfcb
Omar <omar.ssenhaji@gmail.com>
2026-03-14 00:39:13 +0100

    Test products displaying

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./assets/pages/Home";
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to Vendr</h1>
+          <Home />
         </Container>
       </main>
       <Footer />
---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,23 @@
+import { Card, CardText } from "react-bootstrap";
+
+const Product = ({ product }) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </a>
+
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+
+        <CardText as="h3">${product.price}</CardText>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
