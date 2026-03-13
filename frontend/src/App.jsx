8ee74cfc43dc02ed743584aa1f72c404f23ee2fc
Omar <omar.ssenhaji@gmail.com>
2026-03-13 14:03:19 +0100

    Create header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+
 const App = () => {
-  return <h1>Welcome to vendr</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Vendr</h1>
+        </Container>
+      </main>
+    </>
+  );
 };
 
 export default App;
