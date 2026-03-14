8dff5dad687849b854793913944deac23df49da7
Omar <omar.ssenhaji@gmail.com>
2026-03-14 11:23:59 +0100

    Add initial env variables

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
+import "dotenv/config";
 import express from "express";
 import products from "./data/products.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
