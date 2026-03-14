11dcef5d318d0fc4b8ef52a51badbef5018987ce
Omar <omar.ssenhaji@gmail.com>
2026-03-14 11:05:02 +0100

    Start express server on backend

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import products from "./data/products.js";
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+
+  res.json(product);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
