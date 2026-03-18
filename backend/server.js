ce840efb937d19566e4800f42c2ef5b2b07c86c1
Omar <omar.ssenhaji@gmail.com>
2026-03-18 11:56:52 +0100

    Add bcrypt authentication
    Added email and password check for users when they
    log in

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
