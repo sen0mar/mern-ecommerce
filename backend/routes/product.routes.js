e39d8c51f1d89c2ae7f7288925af2c9c74c87c95
Omar <omar.ssenhaji@gmail.com>
2026-03-25 16:24:10 +0100

    Create route and controller for creating a new product (admin)

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -2,10 +2,12 @@ import express from "express";
 import {
   getProducts,
   getProductById,
+  createProduct,
 } from "../controllers/product.controller.js";
 const productRoutes = express.Router();
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-productRoutes.route("/").get(getProducts);
+productRoutes.route("/").get(getProducts).post(protect, admin, createProduct);
 productRoutes.route("/:id").get(getProductById);
 
 export default productRoutes;
