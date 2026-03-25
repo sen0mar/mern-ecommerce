6f664fac067c7659dff9181bf10202c02dcdc2da
Omar <omar.ssenhaji@gmail.com>
2026-03-25 17:05:02 +0100

    Add route and controller for updating a product

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -3,11 +3,15 @@ import {
   getProducts,
   getProductById,
   createProduct,
+  updateProduct,
 } from "../controllers/product.controller.js";
 const productRoutes = express.Router();
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 productRoutes.route("/").get(getProducts).post(protect, admin, createProduct);
-productRoutes.route("/:id").get(getProductById);
+productRoutes
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, admin, updateProduct);
 
 export default productRoutes;
