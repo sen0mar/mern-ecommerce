8e6ccdf48256e08748d7b07f6b954dd647d928a6
Omar <omar.ssenhaji@gmail.com>
2026-03-27 14:33:20 +0100

    Add controller and route in backend  for deleting a product

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -4,6 +4,7 @@ import {
   getProductById,
   createProduct,
   updateProduct,
+  deleteProduct,
 } from "../controllers/product.controller.js";
 const productRoutes = express.Router();
 import { protect, admin } from "../middleware/authMiddleware.js";
@@ -12,6 +13,7 @@ productRoutes.route("/").get(getProducts).post(protect, admin, createProduct);
 productRoutes
   .route("/:id")
   .get(getProductById)
-  .put(protect, admin, updateProduct);
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
 
 export default productRoutes;
