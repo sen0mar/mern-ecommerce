832cc9c0f8d1809e00799b233837de03e15dc85f
Omar <omar.ssenhaji@gmail.com>
2026-03-29 15:33:13 +0200

    Prepare production script

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -5,7 +5,7 @@ import Product from "../models/product.model.js";
 // @route GET /api/products
 // @access Public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 4;
+  const pageSize = process.env.PAGINATION_LIMIT;
   const page = Number(req.query.pageNumber) || 1;
 
   const keyword = req.query.keyword
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssMinify: false,
+  },
   server: {
     proxy: {
       "/api": "http://localhost:5000",
